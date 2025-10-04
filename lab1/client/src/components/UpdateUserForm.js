9cfd708fb2314f9e0d911f61fcbc6c4fd7eea114
MMarzoo <mmarzouk12378@gmail.com>
2025-10-04 15:33:13 +0300

    lab Day2

--- lab1/client/src/components/UpdateUserForm.js
+++ lab1/client/src/components/UpdateUserForm.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useMutation } from "@apollo/client/react";
 import { UPDATE_USER } from "../graphql/mutations";
 import { GET_USERS } from "../graphql/queries";
+import { Button, Form } from "react-bootstrap";
 
 export default function UpdateUserForm({ user }) {
   const [firstName, setFirstName] = useState(user.firstName);
@@ -25,12 +26,45 @@ export default function UpdateUserForm({ user }) {
   };
 
   return (
-    <form onSubmit={handleSubmit} style={{ marginTop: "1rem" }}>
-      <h4>Update {user.firstName}</h4>
-      <input value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-      <input value={age} onChange={(e) => setAge(e.target.value)} type="number" />
-      <input value={companyId} onChange={(e) => setCompanyId(e.target.value)} />
-      <button type="submit">Update</button>
-    </form>
+    <Form
+      onSubmit={handleSubmit}
+      className="border rounded p-3 mt-3 bg-light shadow-sm"
+    >
+      <h5 className="mb-3 text-primary">Update {user.firstName}</h5>
+
+      <Form.Group className="mb-2">
+        <Form.Label>First Name</Form.Label>
+        <Form.Control
+          type="text"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          placeholder="Enter first name"
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-2">
+        <Form.Label>Age</Form.Label>
+        <Form.Control
+          type="number"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+          placeholder="Enter age"
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Company ID</Form.Label>
+        <Form.Control
+          type="text"
+          value={companyId}
+          onChange={(e) => setCompanyId(e.target.value)}
+          placeholder="Enter company ID"
+        />
+      </Form.Group>
+
+      <Button variant="success" type="submit" className="w-100">
+        Update
+      </Button>
+    </Form>
   );
 }
